6b99f1f677790b46f525bdf622d41afe42b04cb5
moury12 <tanzibamouri00@gmail.com>
2025-05-13 13:03:50 +0600

    fix: arg in terms page

--- lib/presentations/profile/views/term_policy_help_page.dart
+++ lib/presentations/profile/views/term_policy_help_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:get/get.dart';
 import 'package:market_place/core/components/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:market_place/core/constants/app_static_strings.dart';
+import 'package:market_place/core/constants/padding_constant.dart';
 import 'package:market_place/presentations/profile/controllers/account_information_controller.dart';
 
 import '../../../core/constants/fontsize_constant.dart';
@@ -16,20 +18,23 @@ class TermsPolicyHelpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomDefaultAppbar(title: arg),
-      body: Obx(() {
-        return AccountInformationController.to.isLoadingPolicy.value
-            ? PaginationLoadingWidget()
-            : HtmlWidget(
-              arg != null && arg == 'terms'
-                  ? '''${AccountInformationController.to.termsModel.value.desc}
-'''
-                  : '''${AccountInformationController.to.policyModel.value.desc}
-''',
-              textStyle: poppinsRegular.copyWith(
-                fontSize: getFontSizeDefault(),
-              ),
-            );
-      }),
+      body: Padding(
+        padding: padding12,
+        child: Obx(() {
+          return AccountInformationController.to.isLoadingPolicy.value
+              ? PaginationLoadingWidget()
+              : HtmlWidget(
+                arg != null && arg == AppStaticStrings.termsAndCondition.tr
+                    ? '''${AccountInformationController.to.termsModel.value.desc}
+        '''
+                    : '''${AccountInformationController.to.policyModel.value.desc}
+        ''',
+                textStyle: poppinsRegular.copyWith(
+                  fontSize: getFontSizeDefault(),
+                ),
+              );
+        }),
+      ),
     );
   }
 }
